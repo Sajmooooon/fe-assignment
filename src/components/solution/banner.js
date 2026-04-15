ffead09e4a235c4fe17244613ad9efb4ba6298e8
Sajmooooon <sajmoooonsk@gmail.com>
2026-04-15 20:32:29 +0200

    style: pridane mixiny a uprava obrazkov

--- src/components/solution/banner.js
+++ src/components/solution/banner.js
@@ -3,24 +3,28 @@ import { button } from "../button";
 import bannerImage from "../../assets/images/main-banner.png";
 import bannerImageMob from "../../assets/images/main-banner-mobile.png";
 
-// Banner button click handler
 const handleBannerClick = (link, linkTarget = "_self") => {
     if (!link) return;
     window.open(link, linkTarget);
 };
 
-// Solution main banner
 export const solutionBanner = (banner) => html`
     <div class="c-solution-banner">
         <div
             class="c-solution-banner__image"
-            style="${banner.imageUrl ? `background-image: url('${banner.imageUrl}')` : ""}"
+            style="
+                --banner-desktop: url(${bannerImage});
+                --banner-mobile: url(${bannerImageMob});
+            "
         ></div>
 
         <div class="c-solution-banner__overlay"></div>
+
         <div class="c-solution-banner__content">
             <h1 class="c-solution-banner__content__title">${banner.title}</h1>
+
             <div class="c-solution-banner__content__description">${banner.description}</div>
+
             ${button({
                 text: banner.ctaText,
                 onClick: () => handleBannerClick(banner.link, banner.linkTarget),
